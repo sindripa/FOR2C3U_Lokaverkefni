bada7524deaa2c020ffb212e8de3984cde7abab1
Sigurðurorri Orri <Sigururorri Orri>
2017-04-19 01:47:54 +0000

    Finished player Deck, AI working, game working, all good and dandy

--- DnDTopTrumps/DnD/DnD/Program.cs
+++ DnDTopTrumps/DnD/DnD/Program.cs
@@ -9,10 +9,161 @@ namespace DnD
 {
     class Program
     {
-        public List<Bass> stokkur;
         static void Main(string[] args)
         {
-            
+            //stocka val
+            bool PlayersTurn = true;
+            short input=8;
+            stockurDnD kek = new stockurDnD();
+            List<Player> stockur = shofle(kek.stockur);
+            List<Player> AiStockur = new List<Player>();
+            List<Player> PlayerStockur = new List<Player>();
+            List<Player> cardPool = new List<Player>();
+            for (int i = 0; i < 52; i++)
+            {
+                if (26>i)
+                {
+                    PlayerStockur.Add(stockur[i]);
+                }
+                else
+                {
+                    AiStockur.Add(stockur[i]);
+                }
+            }
+            while (PlayerStockur.Count > 0 && AiStockur.Count > 0)
+            {
+                if (PlayersTurn)//who picks?
+                {
+                    PlayersTurn = true;
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Player: " + PlayerStockur.Count + " vs AI: " + AiStockur.Count);
+                        Console.WriteLine(PlayerStockur[0]);
+                        Console.WriteLine("\n0)Armor class");
+                        Console.WriteLine("1)Health Points");
+                        Console.WriteLine("2)Strength");
+                        Console.WriteLine("3)Dexterity");
+                        Console.WriteLine("4)Constitution");
+                        Console.WriteLine("5)Intelligence");
+                        Console.WriteLine("6)Wistom");
+                        Console.WriteLine("7)Charisma");
+                        Console.Write("Select the stat(the number): ");
+                        input = Convert.ToInt16(Console.ReadLine());
+                    } while (input < 0 || input > 7);
+                    PlayersTurn = false;
+                }
+                else
+                {
+                    input = AI(AiStockur[0], kek);
+                    PlayersTurn = true;
+                }
+                Console.Clear();
+                if (PlayersTurn)
+                {
+                    switch (input)
+                    {
+                        case 0:
+                            Console.WriteLine("AI picks Armor class");
+                            break;
+                        case 1:
+                            Console.WriteLine("AI picks Health");
+                            break;
+                        case 2:
+                            Console.WriteLine("AI picks Strength");
+                            break;
+                        case 3:
+                            Console.WriteLine("AI picks Dexterity");
+                            break;
+                        case 4:
+                            Console.WriteLine("AI picks Constitution");
+                            break;
+                        case 5:
+                            Console.WriteLine("AI picks Intelligence");
+                            break;
+                        case 6:
+                            Console.WriteLine("AI picks Wistom");
+                            break;
+                        case 7:
+                            Console.WriteLine("AI picks Charisma");
+                            break;
+                        default:
+                            Console.WriteLine("ehhh");
+                            break;
+                    }
+                }
+                Console.WriteLine("Player: "+ PlayerStockur[0].Stats[input]+" vs AI: "+AiStockur[0].Stats[input]);
+                Console.WriteLine(PlayerStockur[0]);
+                Console.WriteLine("vs");
+                Console.WriteLine(AiStockur[0]);
+                Console.ReadKey();
+                if (PlayerStockur[0].Stats[input] == AiStockur[0].Stats[input])
+                {
+                    cardPool.Add(PlayerStockur[0]);
+                    cardPool.Add(AiStockur[0]);
+                    PlayerStockur.Remove(PlayerStockur[0]);
+                    AiStockur.Remove(AiStockur[0]);
+                }
+                else if (PlayerStockur[0].Stats[input] > AiStockur[0].Stats[input])
+                {
+                    PlayerStockur.Add(PlayerStockur[0]);
+                    PlayerStockur.Add(AiStockur[0]);
+                    PlayerStockur.Remove(PlayerStockur[0]);
+                    AiStockur.Remove(AiStockur[0]);
+                    cardPool = shofle(cardPool);
+                    for (int i = 0; i < cardPool.Count();)
+                    {
+                        PlayerStockur.Add(cardPool[0]);
+                        cardPool.Remove(cardPool[0]);
+                    }
+                }
+                else
+                {
+                    AiStockur.Add(AiStockur[0]);
+                    AiStockur.Add(PlayerStockur[0]);
+                    PlayerStockur.Remove(PlayerStockur[0]);
+                    AiStockur.Remove(AiStockur[0]);
+                    cardPool = shofle(cardPool);
+                    for (int i = 0; i < cardPool.Count();)
+                    {
+                        AiStockur.Add(cardPool[0]);
+                        cardPool.Remove(cardPool[0]);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+
+        public static short AI(Player dude, stockurDnD kek)
+        {
+            double[] shift = new double[8];
+            for (int i = 0; i < 8; i++)
+            {
+                shift[i] = dude.Stats[i]-kek.StatsAvg[i];
+            }
+            for (short i = 0; i < 8; i++)
+            {
+                if (shift[i]==shift.Max())
+                {
+                    return i;
+                }
+            }
+            return 1;
+        }
+
+        public static List<Player> shofle(List<Player> kok)
+        {
+            List<Player> oldone = kok;
+            List<Player> newone = new List<Player>();
+            int init;
+            Random rand = new Random();
+            for (int i = 0; i < oldone.Count;)//rennur þangað til að stokkur klárast
+            {
+                init = rand.Next(0, oldone.Count);
+                newone.Add(oldone[init]);
+                oldone.Remove(oldone[init]);
+            }
+            return newone;
         }
     }
 }
--- DnDTopTrumps/TopTrumpsHeros/TopTrumpsHeros/Bass.cs
+++ DnDTopTrumps/TopTrumpsHeros/TopTrumpsHeros/Bass.cs
@@ -17,21 +17,30 @@ namespace TopTrumpsDnD
         public short Wistom { get; private set; }
         public short Constitution { get; private set; }
         public short Charisma { get; private set; }
+        public short[] Stats = new short[8];
         public Bass(string tipa,short ac, short hp, short str,  short dex, short con, short intel, short wis, short cha)
         {
             Tipa = tipa;
             ArmourClass = ac;
+            Stats[0] = ac;
             HitPoints = hp;
+            Stats[1] = hp;
             Strength = str;
-            Intelligence = intel;
+            Stats[2] = str;
             Dexterity = dex;
-            Wistom = wis;
+            Stats[3] = dex;
             Constitution = con;
+            Stats[4] = con;
+            Intelligence = intel;
+            Stats[5] = intel;
+            Wistom = wis;
+            Stats[6] = wis;
             Charisma = cha;
+            Stats[7] = cha;
         }
         public override string ToString()
         {
-            return Tipa + "\nMax Health: " + HitPoints + "\nArmour Class: " + ArmourClass + "\nStrength: " + Strength + "\nDexterity: " + Dexterity + "\nConstitution: " + Constitution + "\nIntelligence: " + Intelligence + "\nWistom: " + Wistom + "\nCharisma: " + Charisma;
+            return Tipa  + "\nArmour Class: " + ArmourClass+ "\nMax Health: " + HitPoints + "\nStrength: " + Strength + "\nDexterity: " + Dexterity + "\nConstitution: " + Constitution + "\nIntelligence: " + Intelligence + "\nWistom: " + Wistom + "\nCharisma: " + Charisma;
         }
     }
     public class Player : Bass
@@ -39,7 +48,7 @@ namespace TopTrumpsDnD
         public string Nafn { get; set; }
         public string Role { get; set; }
         public Player(string nafn,string role,string tipa, short ac, short hp, short str, short dex, short con, short intel, short wis, short cha)
-            : base(tipa, ac, hp, str, intel, dex, wis, con, cha)
+            : base(tipa, ac, hp, str, dex, con, intel, wis, cha)
         {
             Nafn = nafn;
             Role = role;
@@ -51,14 +60,15 @@ namespace TopTrumpsDnD
     }
     public class stockurDnD
     {
+        public List<Player> stockur = new List<Player>();
         public Player[] stockurinn = {  new Player("LightNir", "Rogue/arcane trickster", "Half elf", 14, 13, 12, 17, 10, 16, 12, 10),
                                         new Player("NoLeb", "Druid/Circle of Land", "Human", 10, 15, 17, 9, 13, 13, 17, 10),
                                         new Player("Sindri Páls", "Cleric", "Human", 10, 10, 10, 8, 12, 13, 10, 6),
                                         new Player("Snorri", "Teacher", "Human", 11, 15, 11, 11, 11, 14, 13, 10),
-                                        new Player("Aldrid The Imputent", "Sorcerer", "goblin", 11, 10, 6, 12, 7, 16, 14, 4),
+                                        new Player("Aldrid The Imputent", "Sorcerer", "Ogre", 11, 10, 14, 12, 7, 17, 14, 4),
                                         new Player("Hugombo", "Tank/rock smasher", "Ogre", 14, 18, 19, 10, 14, 5, 6, 4),
-                                        new Player("Nizerus", "Rouge/assasin", "gnome", 16, 8, 9, 18, 7, 14, 12, 12),
-                                        new Player("Melona", "Rouge/thief", "Human", 14, 9, 9, 16, 8, 14, 15, 16),
+                                        new Player("Nizerus", "Rogue/assasin", "gnome", 16, 8, 9, 18, 7, 14, 12, 12),
+                                        new Player("Melona", "Rogue/thief", "Human", 14, 9, 9, 16, 8, 14, 15, 16),
                                         new Player("Bart", "Bard/support", "Dwarf", 13, 15, 12, 10, 13, 14, 15, 16),
                                         new Player("Ombinotus", "Paladin", "Half elf", 15, 18, 14, 10, 16, 17, 13, 14),
                                         new Player("Hercumes", "Barbarian", "Orc", 13, 10, 20, 16, 9, 10, 12, 11),
@@ -66,14 +76,14 @@ namespace TopTrumpsDnD
                                         new Player("Amotabono", "Swordsman", "Human", 15, 10, 16, 16, 12, 13, 14, 11),
                                         new Player("Brock O. Lee", "Monk", "Human", 16, 14, 15, 17, 12, 10, 13, 10),
                                         new Player("Charaka", "Magie", "Elf", 8, 7, 10, 7, 8, 18, 17, 16),
-                                        new Player("DANK MEME", "bard", "Abomination", 1, 1, 10, 1, 1, 12, 12, 12),
+                                        new Player("DANK MEME", "bard", "Abomination", 1, 1, 10, 1, 1, 16, 12, 16),
                                         new Player("Romfus", "Druid", "Dwarf", 14, 12, 14, 13, 12, 13, 14, 12),
-                                        new Player("Dracius", "Wizard", "Dragonite", 13, 11, 11, 9, 11, 20, 16, 12),
+                                        new Player("Dracius", "Wizard", "Dragonborn", 13, 11, 11, 9, 11, 20, 16, 12),
                                         new Player("Brockus", "Cleric", "Half Orc", 16, 12, 14, 9, 14, 12, 12, 13),
                                         new Player("Penriany", "Sorcerer", "Gnome", 14, 9, 7, 16, 8, 16, 15, 14),
                                         new Player("Trunk", "Barbarian", "Gnome", 18, 17, 15, 14, 17, 7, 8, 9),
                                         new Player("GurKursckar", "Warlock", "Orc", 10, 11, 10, 10, 11, 17, 18, 10),
-                                        new Player("Scruff", "Roge", "Goblin", 17, 6, 13, 20, 7, 8, 11, 7),
+                                        new Player("Scruff", "Rogue", "Goblin", 17, 6, 13, 20, 7, 8, 11, 7),
                                         new Player("Jama", "Wizard", "Elf", 8, 8, 9, 8, 8, 18, 18, 15),
                                         new Player("IckSan", "Monk", "Halfling", 14, 11, 14, 15, 11, 10, 11, 15),
                                         new Player("Hexicon", "Lich", "Tiefling", 12, 8, 12, 4, 10, 17, 13, 16),
@@ -83,27 +93,138 @@ namespace TopTrumpsDnD
                                         new Player("Daby", "Cleric", "Dwarf", 15, 14, 14, 6, 13, 15, 12, 12),
                                         new Player("Lingur", "Wizard", "Elf", 11, 8, 7, 12, 7, 18, 15, 7),
                                         new Player("Vermona", "Bard", "Human", 10, 9, 10, 10, 7, 13, 12, 14),
-                                        new Player("Sigurður Orri Hjalta", "Monk", "Human", 12, 8, 10, 13, 9, 13, 9, 7),
+                                        new Player("Sigurður Orri Hjalta", "Monk", "Human", 12, 8, 10, 13, 9, 13, 12, 7),
                                         new Player("Holly", "Sorcerer", "Tiefling", 10, 11, 11, 12, 14, 14, 16, 13),
                                         new Player("Ama'croh", "Fighter", "Dragonborn", 15, 17, 17, 13, 17, 14, 13, 8),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , ),
-                                        new Player("", "", "", , , , , , , , )
-                                     
+                                        new Player("Policom", "Ranger", "Half Elf", 14, 12, 7, 16, 7, 13, 13, 12),
+                                        new Player("HoKing", "Wizard", "Elf", 4, 5, 3, 3, 8, 20, 20, 14),
+                                        new Player("Malkoa", "Monk", "Half Elf", 15, 15, 14, 15, 14, 10, 15, 10),
+                                        new Player("Boraka", "Barbarion", "Half Ork", 8, 16, 15, 7, 16, 7, 8, 14),
+                                        new Player("Alibok", "Druid", "Half Orc", 11, 13, 14, 14, 12, 13, 16, 7),
+                                        new Player("Zena", "Paladin", "Tiefling", 18, 18, 13, 7, 18, 17, 13, 15),
+                                        new Player("Dinean", "Ranger", "Halfling", 16, 9, 10, 18, 9, 12, 16, 14),
+                                        new Player("Jain", "Wizard", "Human", 12, 10, 8, 13, 10, 17, 16, 15),
+                                        new Player("Glandin", "Warlock", "Dwarf", 12, 13, 12, 11, 13, 17, 17, 15),
+                                        new Player("Evana", "Warlock", "Halfling", 14, 10, 9, 16, 11, 14, 14, 17),
+                                        new Player("Mina", "Fighter", "Dwarf", 16, 16, 16, 7, 16, 8, 13, 16),
+                                        new Player("Udi", "Druid", "Half Orc", 12, 12, 8, 13, 14, 13, 15, 13),
+                                        new Player("Reachy", "Fighter", "Gnome", 16, 13, 16, 13, 14, 8, 13, 15),
+                                        new Player("Tari'con", "cleric", "Dragonborn", 16, 16, 13, 12, 14, 16, 16, 15),
+                                        new Player("Quoro", "Fighter", "Orc", 12, 15, 18, 10, 16, 7, 9, 8),
+                                        new Player("Slashon", "Rogue", "Human", 15, 10, 10, 18, 11, 12, 11, 13),
+                                        new Player("Hance", "Cleric", "Half Orc", 18, 9, 10, 7, 9, 17, 15, 15)
                                      };
+        public double AcAvg
+        {
+            get
+            {
+                int[] st = new int[52];
+                for (int i = 0; i < stockur.Count; i++)
+                {
+                    st[i] = stockur[i].ArmourClass;
+                }
+                return st.Average();
+            }
+        }
+        public double HpAvg
+        {
+            get
+            {
+                int[] st = new int[52];
+                for (int i = 0; i < stockur.Count; i++)
+                {
+                    st[i] = stockur[i].HitPoints;
+                }
+                return st.Average();
+            }
+        }
+        public double StrAvg
+        {
+            get
+            {
+                int[] st = new int[52];
+                for (int i = 0; i < stockur.Count; i++)
+                {
+                    st[i] = stockur[i].Strength;
+                }
+                return st.Average();
+            }
+        }
+        public double DexAvg
+        {
+            get
+            {
+                int[] st = new int[52];
+                for (int i = 0; i < stockur.Count; i++)
+                {
+                    st[i] = stockur[i].Dexterity;
+                }
+                return st.Average();
+            }
+        }
+        public double ConAvg
+        {
+            get
+            {
+                int[] st = new int[52];
+                for (int i = 0; i < stockur.Count; i++)
+                {
+                    st[i] = stockur[i].Constitution;
+                }
+                return st.Average();
+            }
+        }
+        public double IntAvg
+        {
+            get
+            {
+                int[] st = new int[52];
+                for (int i = 0; i < stockur.Count; i++)
+                {
+                    st[i] = stockur[i].Intelligence;
+                }
+                return st.Average();
+            }
+        }
+        public double WisAvg
+        {
+            get
+            {
+                int[] st = new int[52];
+                for (int i = 0; i < stockur.Count; i++)
+                {
+                    st[i] = stockur[i].Wistom;
+                }
+                return st.Average();
+            }
+        }
+        public double ChaAvg
+        {
+            get
+            {
+                int[] st = new int[52];
+                for (int i = 0; i < stockur.Count; i++)
+                {
+                    st[i] = stockur[i].Charisma;
+                }
+                return st.Average();
+            }
+        }
+        public double[] StatsAvg = new double[8];
+        public stockurDnD()
+        {
+            for (int i = 0; i < 52; i++)
+            {
+                stockur.Add(stockurinn[i]);
+                StatsAvg[0] = AcAvg;
+                StatsAvg[1] = HpAvg;
+                StatsAvg[2] = StrAvg;
+                StatsAvg[3] = DexAvg;
+                StatsAvg[4] = ConAvg;
+                StatsAvg[5] = IntAvg;
+                StatsAvg[6] = WisAvg;
+                StatsAvg[7] = ChaAvg;
+            }
+        }
     }
 }
